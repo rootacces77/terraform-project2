5165cf65c3580a55731c08610150c43d09202a90
rootacces77 <rootacces77@gmail.com>
2026-01-14 22:28:41 +0100

    Creating us-east-1

--- Network/Stage1/us-east-1/VPC-1/main.tf
+++ Network/Stage1/us-east-1/VPC-1/main.tf
@@ -70,7 +70,7 @@ resource "aws_eip" "nat" {
 
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.vpc_1_public["us_east_1a"].id
+  subnet_id     = aws_subnet.vpc_1_public["us-east-1a"].id
 
   tags = {
     Name = "nat_gw_public_1"
