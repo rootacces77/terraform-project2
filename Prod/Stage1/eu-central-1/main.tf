8d3a9bac28fba53a86dab20d82254d92610c60c8
rootacces77 <rootacces77@gmail.com>
2026-01-16 18:24:08 +0100

    Creating eu-central-1

--- Prod/Stage1/eu-central-1/main.tf
+++ Prod/Stage1/eu-central-1/main.tf
@@ -12,8 +12,8 @@ module "kinesis" {
 module "lambda" {
     source = "./Lambda"
 
-    kinesis_streamA_arn = module.kinesis.kinesis_steam_A_arn
-    kinesis_streamB_arn = module.kinesis.kinesis_steam_B_arn
+    kinesis_streamA_arn = module.kinesis.kinesis_stream_A_arn
+    kinesis_streamB_arn = module.kinesis.kinesis_stream_B_arn
 
     kinesis_streamA_name = module.kinesis.kinesis_stream_A_name
     kinesis_streamB_name = module.kinesis.kinesis_stream_B_name
@@ -42,8 +42,8 @@ module "firehose" {
     bucket_A_arn         = module.s3.destA_s3_arn
     bucket_B_arn         = module.s3.destB_s3_arn
 
-    kinesis_stream_a_arn = module.kinesis.kinesis_steam_A_arn
-    kinesis_stream_b_arn = module.kinesis.kinesis_steam_B_arn
+    kinesis_stream_a_arn = module.kinesis.kinesis_stream_A_arn
+    kinesis_stream_b_arn = module.kinesis.kinesis_stream_B_arn
 
 
   
